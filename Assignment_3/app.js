906540f136090c799b9aa5e686461ff9f56b4ab6
Frank South <frankstonesouth@gmail.com>
2022-07-26 12:25:36 -0400

    Completed prototype methods for map, filter, and reduce

--- Assignment_3/app.js
+++ Assignment_3/app.js
@@ -3,48 +3,48 @@
 const arr = [1,2,3,4];
 
 Array.prototype.ourMap = function(cb){
+    const mapArr = [];
     for(let i = 0; i < this.length; i++){
-        cb(this[i],i);
+        mapArr.push(cb(this[i],i));
     }
+    return mapArr;
 };
 
-arr.ourMap((e,i) => {
-    console.log(`The square of ${e} is ${e*e}, found at index ${i}.`);
-});
+const squared = arr.ourMap(e => (e*e));
+console.log(squared);
 
-//Array.reduce() prototype
 
-// Array.reduce()
-
-const nums = ['4', '3', '3', '7', '9', '5', '6'];
-
-Array.prototype.ourReduce = function(cb){
-    for(let i = 0; i< animals.length; i++ ){
-        cb(this[i], i);
-    }
-};
-
-nums.ourReduce((e,i) => {
-    return e + i;
-});
-
-console.log(total);
-// console.log(value);
 
 //Array.filter() prototype
 
 const animals = ['Tiger', 'Rhino', 'Dog', 'Bear', 'Monkey', 'Giraffe', 'Ape', 'Zebra'];
 
 Array.prototype.ourFilter = function(cb){
-    for(let i = 0; i < animals.length; i++){
-        cb(this[i],i);
+    const filterArr = [];
+    for(let i = 0; i < this.length; i++){
+        if(cb(this[i], i, this)){
+            filterArr.push(this[i]);
+        }
     }
+    return filterArr; 
 };
 
-animals.ourFilter((e,i) => {
-    if(e.length >= 5){
-        console.log(e,i);
-    } else {
-        console.log('Name is too short')
+const filtered = animals.ourFilter(animal => animal.length >=5);
+console.log(filtered);
+
+
+
+//Array.reduce() prototype
+
+const nums = [4, 3, 3, 7, 9, 5, 6];
+
+Array.prototype.ourReduce = function(cb){
+    let total = 0;
+    for(let i = 0; i < this.length; i++ ){
+        total = cb(total, this[i]);
     }
-});
+    return total;
+};
+
+const summed = nums.ourReduce((prev,init) => prev + init);
+console.log(summed);
